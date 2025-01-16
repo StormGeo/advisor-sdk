453d69d6994c6bf8ca526fec2017b2b570aaa6b0
ThiagoOliveiraAires <thiago.oliveira@climatempo.com.br>
2025-01-16 16:38:12 -0300

    fix: Update setup configuration

--- python-advisor-core/setup.py
+++ python-advisor-core/setup.py
@@ -8,6 +8,7 @@ setup(
     license = 'MIT',
     description = 'SDK to access the advisor core API.',
     long_description = open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url = 'https://github.com/StormGeo/advisor-sdk',
     project_urls = {
         'Código fonte': 'https://github.com/StormGeo/advisor-sdk/tree/main/python-advisor-core'
