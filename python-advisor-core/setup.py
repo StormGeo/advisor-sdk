ae1562ae6189380de327e6deae7079d6ae5b2d07
José Mariano da Silva <jose.mariano@climatempo.com.br>
2025-01-16 17:15:47 -0300

    chore: Change python sdk access point

--- python-advisor-core/setup.py
+++ python-advisor-core/setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'stormgeo.advisor-core',
-    version = '0.1.1',
+    version = '0.1.2',
     author = 'climatempo',
-    packages = find_packages(include=['stormgeo.advisor_core']),
+    packages = find_packages(include=['advisor_core']),
     license = 'MIT',
     description = 'SDK to access the advisor core API.',
     long_description = open("README.md", "r", encoding="utf-8").read(),
