da5dd6b0b844bbd64895c3598d1fce8e34d9f06b
Raul Araujo Machado <raul.machado@climatempo.com.br>
2025-01-21 10:24:05 -0300

    chore: Update version

--- python-advisor-core/setup.py
+++ python-advisor-core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'stormgeo.advisor-core',
-    version = '0.1.2',
+    version = '0.1.3',
     author = 'climatempo',
     packages = find_packages(include=['advisor_core']),
     license = 'MIT',
