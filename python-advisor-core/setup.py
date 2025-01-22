114bb36a6c94fb5d55026b5abdb9f2ffe45180b8
ThiagoOliveiraAires <thiago.oliveira@climatempo.com.br>
2025-01-22 16:13:57 -0300

    chore: Update python sdk version

--- python-advisor-core/setup.py
+++ python-advisor-core/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'stormgeo.advisor-core',
-    version = '0.1.3',
+    version = '1.0.0',
     author = 'climatempo',
     packages = find_packages(include=['advisor_core']),
     license = 'MIT',
